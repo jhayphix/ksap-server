454e80cf75c54c1ccc93d00ae6b746ced1312be2
jhayphix <jhayphix05@gmail.com>
2025-04-28 15:26:34 +0100

    Initial commits

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from 'mongoose';
 import path from "path";
 import { fileURLToPath } from "url";
 import scholarshipRoutes from "./routes/scholarshipRoutes.js";
@@ -7,12 +8,29 @@ import errorHandler from "./middleware/error.js";
 import notFound from "./middleware/notFound.js";
 
 const port = process.env.PORT || 8000;
+const MONGO_URI = process.env.MONGO_URI
 
 // Get the directory name
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
+// Connect to MongoDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 // Body parser middleware
 app.use(express.json());
@@ -21,7 +39,7 @@ app.use(express.urlencoded({ extended: false }));
 // Logger middleware
 app.use(logger);
 
-// setup static folder
+// Setup static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
