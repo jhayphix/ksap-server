792277e64a2d515759c8e28650f755fb1adf4295
jhayphix <jhayphix05@gmail.com>
2025-04-28 21:40:26 +0100

    Work on scholarship controller

--- models/scholarshipModel.js
+++ models/scholarshipModel.js
@@ -32,20 +32,21 @@ const applicationSectionSchema = new mongoose.Schema({
 
 // Main Scholarship Schema
 const scholarshipSchema = new mongoose.Schema({
-  createdByAdminId: { type: String, required: true },
-  name: { type: String, required: true },
-  shortName: { type: String, required: true },
-  fundingType: { type: String, required: true },
   academicYear: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-  deadline: { type: Date, required: true },
-  eligibilityYearsOfStudy: { type: [String], required: true },
-  eligibilityEducationalLevel: { type: [String], required: true },
-  requirements: { type: [String], required: true },
   applicationSections: [applicationSectionSchema], // Reference to applicationSection schema
+  createdAt: { type: Date, default: Date.now },
+  createdByAdminId: { type: String, required: true },
+  deadline: { type: Date, required: true },
   description: { type: String, default: "" },
+  eligibilityEducationalLevel: { type: [String], required: true },
+  eligibilityYearsOfStudy: { type: [String], required: true },
   externalId: { type: String, required: true },
+  fundingType: { type: String, required: true },
+  name: { type: String, required: true },
+  requirements: { type: [String], required: true },
+  shortName: { type: String, required: true },
+  updatedAt: { type: Date, default: Date.now },
+  updatedByAdminId: { type: String, required: true },
 });
 
 // Create Scholarship model
